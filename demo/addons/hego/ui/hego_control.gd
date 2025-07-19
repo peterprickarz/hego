bc29ad4788427c3088e9b78646d7fdaff15277f1
Chris Sprance <chrissprance@gmail.com>
2025-07-19 16:09:50 -0400

    Update hego control with new UI to allow for more logging related to sessions

--- demo/addons/hego/ui/hego_control.gd
+++ demo/addons/hego/ui/hego_control.gd
@@ -3,28 +3,31 @@ extends Control
 
 signal selected_hego_node_changed(node: Node)
 
+@onready var start_button: Button = %ButtonStartSession
+@onready var stop_button: Button = %ButtonStopSession
+@onready var connection_type: OptionButton = %ConnectionType
+@onready var connection_data: TextEdit = %ConnectionData
+@onready var logs: TextEdit = %Logs
+
 var hego_tool_node: Node
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var start_button = $"TabContainer/Houdini Engine/ButtonStartSession"
-	var stop_button = $"TabContainer/Houdini Engine/ButtonStopSession"
 	start_button.pressed.connect(_on_start_session_button_pressed)
 	stop_button.pressed.connect(_on_stop_session_button_pressed)
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 	
-func update_hego_asset_node(node:Node):
+func update_hego_asset_node(node: Node):
 	hego_tool_node = node
 	selected_hego_node_changed.emit(node)
-	
+
+
 func _on_start_session_button_pressed():
+	# Stop it then start it again to ensure a clean session
+	HEGoAPI.get_singleton().stop_session()
 	HEGoAPI.get_singleton().start_session()
-	
+
+
 func _on_stop_session_button_pressed():
 	HEGoAPI.get_singleton().stop_session()
-	
